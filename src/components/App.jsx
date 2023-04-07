30c94603f10dc2cc35f31bcd009f7826ea153c4c
oleg-torba <100319055+oleg-torba@users.noreply.github.com>
2023-04-07 23:01:52 +0300

    profile

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,20 @@
+import { Profile } from "./profile/profile";
+import user from './user.json'
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+   
+    
+     <Profile 
+     username={user.username}
+     tag={user.tag}
+    location={user.location}
+    avatar={user.avatar}
+    stats = {user.stats}
+    followers = {user.stats.followers}
+    views = {user.stats.views}
+    likes = {user.stats.likes}
+    />
+   
   );
 };
---
+++ src/components/profile/profile.jsx
@@ -0,0 +1,42 @@
+import PropTypes from 'prop-types';
+import profileCss from './profile.module.css';
+
+
+export function Profile({ avatar, username, tag, location, followers, views, likes }) {
+  return (
+    <>
+      <div className={profileCss.profile}>
+        <div className={profileCss.description}>
+          <img className={profileCss.img} src={avatar} alt="img" />
+          <h1 className={profileCss.title}>{username}</h1>
+          <p className={profileCss.location}>@{tag}</p>
+          <p className={profileCss.location}>{location}</p>
+        </div>
+        <ul className={profileCss.stats}>
+          <li className={profileCss.item}>
+            <span className={profileCss.itemTitle}>Followers</span>
+            <span className={profileCss.itemValue}>{followers}</span>
+          </li>
+          <li className={profileCss.item}>
+            <span className={profileCss.itemTitle}>Views</span>
+            <span className={profileCss.itemValue}>{views}</span>
+          </li>
+          <li className={profileCss.item}>
+            <span className={profileCss.itemTitle}>Likes</span>
+            <span className={profileCss.itemValue}>{likes}</span>
+          </li>
+        </ul>
+      </div>
+    </>
+  );
+}
+
+Profile.propTypes= {
+    avatar: PropTypes.string.isRequired,
+username:PropTypes.string.isRequired,
+tag: PropTypes.string.isRequired, 
+location: PropTypes.string.isRequired,
+followers: PropTypes.number.isRequired,
+views: PropTypes.number.isRequired,
+likes: PropTypes.number.isRequired
+}
